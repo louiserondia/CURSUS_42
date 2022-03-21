4508ff063eaa8dc42cdcf07a66e0467cc3407ee5
Louise Rondia <louiserondia@gmail.com>
2022-03-21 11:19:24 +0100

    exam rank 2

--- EXAM_RANK_2/gnl_exam/get_next_line.c
+++ EXAM_RANK_2/gnl_exam/get_next_line.c
@@ -3,114 +3,146 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrondia <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lrondia <lrondia@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/15 12:34:43 by lrondia           #+#    #+#             */
-/*   Updated: 2022/02/15 18:37:44 by lrondia          ###   ########.fr       */
+/*   Created: 2022/03/16 12:13:11 by lrondia           #+#    #+#             */
+/*   Updated: 2022/03/16 12:55:40 by lrondia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	new_buffer(char *buffer, char *line)
+int	ft_strchr(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
 	int		j;
+	char	*str;
 
 	i = 0;
 	j = 0;
-	while (line[i] && line[i] != '\n')
-		i++;
-	if (line[i] == '\n')
-		i++;
-	while (line[i] && j < BUFFER_SIZE - 1)
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc (sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	while (s1[i])
 	{
-		buffer[j] = line [i];
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	free (s1);
+	return (str);
+}
+#include <stdio.h>
+
+char	*get_line(char *temp)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	j = 0;
+	while (temp[i] && temp[i] != '\n')
+		i++;
+	if (temp[i] == '\n')
+		i++;
+	line = malloc(sizeof(char) * i + 1);
+	while (j < i)
+	{
+		line[j] = temp[j];
+		j++;
+	}
+	line[j] = '\0';
+	return (line);
+}
+
+void	get_buffer(char *temp, char *buffer)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (temp[i] && temp[i] != '\n')
+		i++;
+	if (temp[i] == '\n')
+		i++;
+	while (temp[i])
+	{
+		buffer[j] = temp[i];
 		i++;
 		j++;
 	}
 	buffer[j] = '\0';
 }
 
-char	*new_line(char *line)
+char	*get_next_line(int fd)
 {
-	int		i;
-	char	*new_line;
+	int			nbytes;
+	char		*line;
+	char		*temp;
+	char		buf[BUFFER_SIZE + 1];
+	static char	buffer[BUFFER_SIZE + 1];
 
-	i = 0;
-	while (line[i] != '\n')
-		i++;
-	if (line[i] == '\n')
-		i++;
-	new_line = malloc(sizeof (char) * i + 1);
-	if (!new_line)
-		return (NULL);
-	i = 0;
-	while (line[i] && line[i] != '\n')
-	{
-		new_line[i] = line[i];
-		i++;
-	}
-	if (line[i] == '\n')
-	{
-		new_line[i] = line[i];
-		i++;
-	}
-	new_line[i] = '\0';
-	free (line);
-	return (new_line);
-}
-
-char	*read_line(int fd, char	*line)
-{
-	int		nbytes;
-	char	*temp;
-
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	   	return (NULL);
 	nbytes = BUFFER_SIZE;
-	while (nbytes == BUFFER_SIZE && !ft_strchr('\n', line))
+	buf[0] = '\0';
+	temp = malloc(sizeof (char) * 1);
+	temp[0] = '\0';
+	temp = ft_strjoin(temp, buffer);
+	while (nbytes > 0 && !ft_strchr(temp, '\n'))
 	{
-		temp = malloc (sizeof (char) * BUFFER_SIZE + 1);
-		if (!temp)
-			return (NULL);
-		nbytes = read(fd, temp, BUFFER_SIZE);
+		nbytes = read(fd, buf, BUFFER_SIZE);
 		if (nbytes == -1)
 		{
-			free (line);
 			free (temp);
 			return (NULL);
 		}
-		temp[nbytes] = '\0';
-		line = ft_strjoin(line, temp);
-		free (temp);
-		if (!line)
-			return (NULL);
+		buf[nbytes] = '\0';
+		temp = ft_strjoin(temp, buf);
 	}
-	if (!temp[0] && !line[0])
+	if (nbytes == 0 && temp[0] == '\0')
 	{
-		free (line);
+		free (temp);
 		return (NULL);
-	}	
+	}
+	line = get_line(temp);
+	get_buffer(temp, buffer);
+	free (temp);
 	return (line);
+
 }
 
-char	*get_next_line(int fd)
-{
-	char		*line;
-	static char	buffer[BUFFER_SIZE + 1];
-
-	if (BUFFER_SIZE == 0)
-		return (NULL);
-	line = malloc(sizeof (char) * 1);
-	if (!line)
-		return (NULL);
-	line[0] = '\0';
-	line = ft_strjoin(line, buffer);
-	if (!line)
-		return (NULL);
-	line = read_line(fd, line);
-	if (!line)
-		return (NULL);
-	new_buffer(buffer, line);
-	line = new_line(line);
-	return (line);
-}
--- EXAM_RANK_2/gnl_exam/main.c
+++ EXAM_RANK_2/gnl_exam/main.c
@@ -1,22 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lrondia <lrondia@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/15 13:30:08 by lrondia           #+#    #+#             */
-/*   Updated: 2022/03/15 14:29:01 by lrondia          ###   ########.fr       */
+/*   Created: 2022/03/15 13:32:06 by lrondia           #+#    #+#             */
+/*   Updated: 2022/03/16 12:54:04 by lrondia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
+int	main(void)
+{
+	int	fd;
+	char *line;
 
-char	*get_next_line(int fd);
-
-#endif
+	fd = open("test.txt", O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free (line);
+	}
+	// system("leaks a.out");
+}
