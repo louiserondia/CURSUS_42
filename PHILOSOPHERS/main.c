686ae0f00c09249d90fc06c379529376efc92fcc
Louise Rondia <louiserondia@gmail.com>
2022-04-29 14:45:39 +0200

    ok

--- PHILOSOPHERS/main.c
+++ PHILOSOPHERS/main.c
@@ -6,7 +6,7 @@
 /*   By: lrondia <lrondia@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 15:17:22 by lrondia           #+#    #+#             */
-/*   Updated: 2022/04/27 17:20:44 by lrondia          ###   ########.fr       */
+/*   Updated: 2022/04/29 14:43:54 by lrondia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,13 @@ int	main(int argc, char **argv)
 	if (argc < 4 || argc > 5)
 		return (0);
 	table.nb_philo = atoi(argv[1]);
-	table.philo = malloc(sizeof(t_philo) * table.nb_philo);
+	table.philo = malloc(sizeof(t_philo **) * table.nb_philo);
 	if (!table.philo)
 		return (0);
 	memset(&table, 0, sizeof(table));
-	if (argc % 2 == 0)
+	// if (table.philo)
+		printf("%d\n", table.philo[0]->state);
+	if (table.nb_philo % 2 == 0)
 		handle_even_nb_philo(&table, argv);
 	ft_printf_state(&table);
 }
